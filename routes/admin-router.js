e082f5a8c799bde11faba35982d7f771e946f364
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-07-04 15:29:23 +0200

    verified user list for admin, button to verify

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -3,18 +3,31 @@ const passport = require("passport");
 const router = express.Router();
 const User = require("../models/user-model.js");
 
-router.get("/activate-users", (req, res, next) => {
+router.get("/verify-users", (req, res, next) => {
 
-  User.find({accountState: "inactive"})
+  User.find({accountStatus: "unverified"})
   .then((users)=>{
     res.locals.users = users;
-    res.render("/admin-views/activate-users.hbs")
+    res.render("admin-views/activate-users.hbs")
   })
-  .catch(()=> {
-
+  .catch((err)=> {
+    next(err);
   })
-
 })
 
+router.post("/verify-user/:userId", (req, res, next) => {
+  const { userId } = req.params;
+
+  User.findByIdAndUpdate(
+    userId,
+    { $set: {accountStatus: "verified"} }
+  )
+  .then(() => {
+    res.redirect('/verify-users');
+  })
+  .catch((err) => {
+    next(err);
+  })
+})
 
 module.exports = router;
