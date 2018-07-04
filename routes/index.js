cb93a076a03f6d293dfe78689fd5995d59829b8b
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-07-04 21:38:29 +0200

    change on index for admins

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,16 @@ const User = require("../models/user-model.js");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-router.get('/random', (req, res, next) => {
-  User.find()
-  .then((userDoc)=>{
-    const random = Math.floor(Math.random() * userDoc.length)
-    const hackerDoc = userDoc[random];
-    res.locals.hackerDoc = hackerDoc;
-    res.redirect(`/find-hackers/${hackerDoc._id}`)
+  User.count({accountStatus: "unverified"})
+  .then((number)=> {
+    res.locals.verifyCount = number;
+    res.render('index');
   })
-  .catch((err)=>{
+  .catch((err) => {
     next(err);
   })
-})
+});
+
+
 
 module.exports = router;
